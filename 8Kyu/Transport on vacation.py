97a94d77f2b9241ade7447990275239fabcc5166
athasv <ath.arvanitidis@gmail.com>
2021-11-25 01:52:45 +0100

    Delete Transport on vacation.py

--- 8Kyu/Transport on vacation.py
+++
@@ -1,5 +0,0 @@
-def rental_car_cost(d):
-    total = d * 40
-    return total - 50 if d >= 7 else (total - 20 if d >= 3 else total)
-    
-    temp.append(3) if i[0] > i[1] else (temp.append(1) if i[0] == i[1] else temp)
