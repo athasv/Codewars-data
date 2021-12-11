88987f04e3e70062513acf40730dd1b7b7f834de
athasv <ath.arvanitidis@gmail.com>
2021-12-11 22:28:40 +0100

    Update
    Update

---
+++ 8Kyu/Convert a String to a Number.py
@@ -0,0 +1,3 @@
+def string_to_number(s):
+    if float(s) == True: return float(s)
+    if not float(s) == True: return int(s)
---
+++ 8Kyu/The Wide-Mouthed frog!.py
@@ -0,0 +1,2 @@
+def mouth_size(animal): 
+    return '{}'.format("small") if animal.lower() == "alligator" else '{}'.format("wide")
