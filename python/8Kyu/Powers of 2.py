363245681ce6d12a7c5e274dab2a0f9a4fa8cbcc
athasv <ath.arvanitidis@gmail.com>
2022-03-31 21:21:39 +0200

    Update
    Update

---
+++ python/8Kyu/Powers of 2.py
@@ -0,0 +1,3 @@
+import math
+def powers_of_two(n):
+    return [ math.pow(2, e) for e in range(n+1) ]
---
+++ python/8Kyu/Semi-Optional.py
@@ -0,0 +1,4 @@
+def wrap(value):
+    my_dict={}
+    my_dict["value"] = value
+    return my_dict
